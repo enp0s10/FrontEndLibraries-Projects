41ed9100b11c62787fb4b245ab89d9ef633aa38e
Adrian Hernandez <65986643+enp0s10@users.noreply.github.com>
2023-04-14 17:11:09 -0700

    Removed Functionality, Recreated Component
    Noticed allot fo tutorials were using a different kind of syntax for their react projects. Using an ES6 function rather than the Class object component.
    
    Rewrote the component, which will allow me to use other features, such as useEffect. Dont have time to work on functionality, will continue that another time. Planning on using event listeners in the useEffect method.
    
    Worst case, I have the previous rollback.

--- drum-machine/src/index.js
+++ drum-machine/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useRef } from 'react';
+import { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
@@ -20,95 +20,93 @@ const audioX = new Audio("https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1
 const audioC = new Audio("https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3");
 
 
-class DrumPad extends React.Component {
+const DrumPad = () => {
+  const [audio] = useState('Welcome!');
   // constructor
-  constructor (props) {
-    super(props);
-    this.state = {
-      audio: 'Welcome!'
-    }
-    this.playAudio = this.playAudio.bind(this);
-  }
-
-  playAudio(letter) {
-    var audio = this.refs.audioQ;
-    switch(letter) {
-      case 'Q':
-        audio = this.refs.audioQ;
-        break;
-      case 'W':
-        audio = this.refs.audioW;
-      default:
-        break;
-    }
-    audio.play();
-  }
-
-  render() {
+  // constructor (props) {
+  //   super(props);
+  //   this.state = {
+  //     audio: 'Welcome!'
+  //   }
+  //   this.playAudio = this.playAudio.bind(this);
+  // }
 
 
-    return (
-      <div className='row justify-content-center'>
-        <div id='drum-machine' className='col-md-6'>
-          <h1 className="text-center page-title">Drum Machine</h1>
-          <div className='row'>
-            {/* Drum Pad */}
-            {/* Row Q,W,E */}
-            <div className='col-md-8 text-center'>
-              <div className='row'>
-                <div id='heater-1' className='drum-pad' onClick={() => this.playAudio('Q')}>
-                  Q
-                  <audio ref="audioQ" id="Q" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
-                </div>
-                <div id='heater-2' className='drum-pad' onClick={() => this.playAudio('W')}>
-                  W
-                  <audio ref="audioW" id="W" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
-                </div>
-                <div id='heater-3' className='drum-pad'>
-                  E
-                  <audio id="E" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
-                </div>
+  // playAudio(letter) {
+  //   var audio = this.refs.audioQ;
+  //   switch(letter) {
+  //     case 'Q':
+  //       audio = this.refs.audioQ;
+  //       break;
+  //     case 'W':
+  //       audio = this.refs.audioW;
+  //     default:
+  //       break;
+  //   }
+  //   audio.play();
+  // }
+
+  return (
+    <div className='row justify-content-center'>
+      <div id='drum-machine' className='col-md-6'>
+        <h1 className="text-center page-title">Drum Machine</h1>
+        <div className='row'>
+          {/* Drum Pad */}
+          {/* Row Q,W,E */}
+          <div className='col-md-8 text-center'>
+            <div className='row'>
+              <div id='heater-1' className='drum-pad'>
+                Q
+                <audio id="Q" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"></audio>
               </div>
-              {/* Row A,S,D */}
-              <div className='row'>
-                <div id='heater-4' className='drum-pad'>
-                  A
-                  <audio id="A" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
-                </div>
-                <div id='clap' className='drum-pad'>
-                  S
-                  <audio id="S" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
-                </div>
-                <div id='open-hh' className='drum-pad'>
-                  D
-                  <audio id="D" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
-                </div>
+              <div id='heater-2' className='drum-pad'>
+                W
+                <audio id="W" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"></audio>
               </div>
-              {/* Row Z,X,C */}
-              <div className='row'>
-                <div id='kick-n-hat' className='drum-pad'>
-                  Z
-                  <audio id="Z" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio>
-                </div>
-                <div id='kick' className='drum-pad'>
-                  X
-                  <audio id="X" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
-                </div>
-                <div id='closed-hh' className='drum-pad'>
-                  C
-                  <audio id="C" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
-                </div>
+              <div id='heater-3' className='drum-pad'>
+                E
+                <audio id="E" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"></audio>
               </div>
             </div>
-            {/* Display */}
-            <div id='display' className='col-md-4 text-center display'>
-              {this.state.audio}
+            {/* Row A,S,D */}
+            <div className='row'>
+              <div id='heater-4' className='drum-pad'>
+                A
+                <audio id="A" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"></audio>
+              </div>
+              <div id='clap' className='drum-pad'>
+                S
+                <audio id="S" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"></audio>
+              </div>
+              <div id='open-hh' className='drum-pad'>
+                D
+                <audio id="D" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"></audio>
+              </div>
             </div>
+            {/* Row Z,X,C */}
+            <div className='row'>
+              <div id='kick-n-hat' className='drum-pad'>
+                Z
+                <audio id="Z" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"></audio>
+              </div>
+              <div id='kick' className='drum-pad'>
+                X
+                <audio id="X" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"></audio>
+              </div>
+              <div id='closed-hh' className='drum-pad'>
+                C
+                <audio id="C" className='clip' src="https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"></audio>
+              </div>
+            </div>
+          </div>
+          {/* Display */}
+          <div id='display' className='col-md-4 text-center display'>
+            {audio}
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 } // DrumPad
 
 
